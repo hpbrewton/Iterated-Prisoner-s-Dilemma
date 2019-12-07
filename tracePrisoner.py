53022c4ed3aa50c11a2395ce4fb6b74ed7b89b23
TraceCarrasco <messagetrace@gmail.com>
2019-12-07 08:41:36 -0600

    cleaned up file names to what I think wants to be done

--- tracePrisoner.py
+++ tracePrisoner.py
@@ -1,14 +1,18 @@
 class Prisoner:
+	previousEnemyMove = "split"	
+
 	def __init__(self):
 		pass
 
 	# takes the string "steal" or "split" representing the other player's move
 	# result is ingored
 	def result(self, other):
-		pass
+		previousEnemyMove = other	
+	
 
 	# return the string "steal" or "split" representing your move
 	def play(self):
-		pass
-
-
+		if previousEnemyMove == "split":
+			return "split"
+		
+		return "steal"
